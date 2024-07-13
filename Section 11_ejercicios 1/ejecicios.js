088c46c77955f9b88a12e6f43443cd4b1fe81f62
P4blitos <124176699+P4blitos@users.noreply.github.com>
2024-07-12 23:01:39 -0500

    ejercicio 1

---
+++ Section 11_ejercicios 1/ejecicios.js
@@ -0,0 +1,19 @@
+'use strict'
+/* Ejercicio que pida dos numeros y que diga cual es mayor, menor o iguales */
+
+let num1= parseInt(prompt("Digita el primer numero", 0)) ;
+let num2= parseInt(prompt("Digita el segundo numero", 0)) ;
+while(num1 <= 0  || num2 <= 0 || isNaN(num1) || isNaN(num2)){
+    num1= parseInt(prompt("Digita el primer numero", 0)) ;
+    num2= parseInt(prompt("Digita el segundo numero", 0)) ;
+}
+
+if(num1 > num2){
+    console.log("El numero mayor es: "+ num1 + "\n" + "El numero menor es: "+ num2);
+}else if(num2 > num1){
+    console.log("El numero mayor es: "+ num2 + "\n" + "El numero menor es: "+ num1)
+}else if(num1==num2){
+    console.log("Los dos numeros son iguales: "+ num1);
+}else{
+    console.log("escribe numeros correctos");
+}
